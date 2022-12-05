d89af934fee1f1c0fd0814256e0eca5d7840a704
poker555 <brad901017@gmail.com>
2022-12-05 13:14:42 +0800

    計算價錢

--- calculate.js
+++ calculate.js
@@ -1,4 +1,19 @@
-if(d )
-{
+var total = 0
+var data = []
 
+
+
+function Order(){
+    
+    document.getElementById("food")
+    var money = parseInt(m)
+    total = total+money
+    document.getElementById('tm').textContent=total+"元"
+    for(i=0;i<data.length;i++)
+    var n = data.push(foodtype)
+    {
+        var node = document.createElement('li')
+        node.appendChild(data[i])
+        document.querySelector('ul').appendChild(node)
+    }
 }
